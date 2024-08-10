cb2dfcdaa2a46dd22cbac4313455ab3d34acb1b1
DanielAzeved0 <Daniel.azevedo081205@gmail.com>
2024-08-09 21:41:38 -0300

    Primeira pagina web

---
+++ Configuração-git.bat
@@ -0,0 +1,14 @@
+echo ON
+
+echo --- Configurando Usuário GiuHUB
+echo
+echo 
+git config --global user.name "DanielAzeved0"
+git config --global user.email Daniel.azevedo081205@gmail.com
+echo ---Mostrando a configuração atual do GitHUB no computador
+git config list
+echo 
+echo
+echo ---Fim!---
+
+pause
